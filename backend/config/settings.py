7f16fb1a949ed54d18bb554a0b396701f7dc2363
RezaSanagoo <rezasanagoo021@gmail.com>
2026-02-12 22:55:41 +0330

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://194.60.231.96:5173",
+    "http://194.60.231.96:8000",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -146,6 +147,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://194.60.231.96:5173",
+    "http://194.60.231.96:8000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -158,6 +160,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://194.60.231.96:5173",
+    "http://194.60.231.96:8000",
 ]
 CSRF_COOKIE_SECURE = False  # Development only
 CSRF_COOKIE_HTTPONLY = False  # Allow JS to read CSRF token
